35a689c3a5532a1eb24824eab97000bf4d8a12ed
VLMH <victorboxster@gmail.com>
2018-05-05 16:54:51 +0800

    validate pet is in supported species

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,4 @@
 class Pet < ApplicationRecord
   validates_presence_of :name, :species
+  validates_inclusion_of :species, in: SPECIES
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Pet, type: :model do
+  # validations
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:species) }
+  it { should validate_inclusion_of(:species).in_array(SPECIES) }
 end
