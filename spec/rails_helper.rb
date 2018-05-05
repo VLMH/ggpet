03fd86d4c68d6b95915acbde36803c9434717a0a
VLMH <victorboxster@gmail.com>
2018-05-05 16:00:06 +0800

    fix spec config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,8 @@
+# require database cleaner at the top level
+require 'database_cleaner'
+require 'factory_bot'
+require 'shoulda-matchers'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -54,4 +59,23 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # add `FactoryBot` methods
+  config.include FactoryBot::Syntax::Methods
+
+  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+
+    FactoryBot.find_definitions
+  end
+end
+
+# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
