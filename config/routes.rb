ebbbbcd913f8879bd48c91a2b5e7e2fc73f7f10e
VLMH <victorboxster@gmail.com>
2018-05-06 09:22:46 +0800

    create pet endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope :v1 do
+    resources :pets, only: [:index, :show, :create] do
+      member do
+        get "matches"
+      end
+    end
+  end
 end
