3a776fa4ec7022d89ccfa38186dc9cfac837203c
VLMH <victorboxster@gmail.com>
2018-05-06 09:57:56 +0800

    revise migration to have unsigned integer for age

--- db/migrate/20180505035331_create_pets.rb
+++ db/migrate/20180505035331_create_pets.rb
@@ -2,7 +2,7 @@ class CreatePets < ActiveRecord::Migration[5.2]
   def change
     create_table :pets do |t|
       t.string :name, null: false, default: ''
-      t.integer :age
+      t.integer :age, :unsigned => true
       t.string :species, null: false, default: ''
       t.string :breed, null: false, default: ''
       t.datetime :available_at
