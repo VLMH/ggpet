90823581df5d3a89b2162650f40bebfaad8bc631
VLMH <victorboxster@gmail.com>
2018-05-06 10:08:13 +0800

    age is an optional field

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,5 @@
 class Pet < ApplicationRecord
   validates_presence_of :name, :species
   validates_inclusion_of :species, in: PET_SPECIES
-  validates_numericality_of :age, only_integer: true
+  validates_numericality_of :age, only_integer: true, allow_nil: true
 end
