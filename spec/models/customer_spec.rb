8a4b68d9a084f5bab94aa3eb08405e8a73c333e1
Victor Lam <victorboxster@gmail.com>
2018-05-07 13:24:09 +0800

    Revert "create customer preference model and associate with customer"
    This reverts commit c9fac785219f0ea12c509f9b0ca6a632418b20cf.

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  context "validations" do
-    it { should validate_presence_of(:name) }
-  end
-
-  context "associations" do
-    it { should have_many(:customer_preferences) }
-  end
+  it { should validate_presence_of(:name) }
 end
