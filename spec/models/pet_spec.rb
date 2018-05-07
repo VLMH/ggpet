bee2632963a9fbc52ba67ce5f280695172170d5e
VLMH <victorboxster@gmail.com>
2018-05-08 07:14:18 +0800

    make breed and age required field

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Pet, type: :model do
   # validations
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:species) }
+  it { should validate_presence_of(:breed) }
+  it { should validate_presence_of(:age) }
   it { should validate_inclusion_of(:species).in_array(PET_SPECIES) }
   it { should validate_numericality_of(:age) }
 end
