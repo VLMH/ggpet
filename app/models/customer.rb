1e783e052f9c11cfceca2e0940924d20caeb61d5
Victor Lam <victorboxster@gmail.com>
2018-05-07 13:24:09 +0800

    Revert "create customer preference model and associate with customer"
    This reverts commit c9fac785219f0ea12c509f9b0ca6a632418b20cf.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,3 @@
 class Customer < ApplicationRecord
-  has_many :customer_preferences
-
   validates_presence_of :name
 end
--- app/models/customer_preference.rb
+++
@@ -1,7 +0,0 @@
-class CustomerPreference < ApplicationRecord
-  belongs_to :customer
-
-  validates_presence_of :species
-  validates_inclusion_of :species, in: PET_SPECIES
-  validates_numericality_of :age, only_integer: true, allow_nil: true
-end
