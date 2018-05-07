ba56e44dacda7f807c1c9cc7933000880ef51a84
VLMH <victorboxster@gmail.com>
2018-05-07 13:46:22 +0800

    set preference default empty array

--- db/migrate/20180506093541_create_customers.rb
+++ db/migrate/20180506093541_create_customers.rb
@@ -2,7 +2,7 @@ class CreateCustomers < ActiveRecord::Migration[5.2]
   def change
     create_table :customers do |t|
       t.string :name
-      t.jsonb :preferences
+      t.jsonb :preferences, default: []
 
       t.timestamps
     end
