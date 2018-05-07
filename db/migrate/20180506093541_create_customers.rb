7a9acab0450c85a4529650f295de87292538477d
VLMH <victorboxster@gmail.com>
2018-05-07 16:44:25 +0800

    preference should be nullable for no preference

--- db/migrate/20180506093541_create_customers.rb
+++ db/migrate/20180506093541_create_customers.rb
@@ -2,7 +2,7 @@ class CreateCustomers < ActiveRecord::Migration[5.2]
   def change
     create_table :customers do |t|
       t.string :name, null: false
-      t.jsonb :preferences, null: false, default: []
+      t.jsonb :preferences
 
       t.timestamps
     end
