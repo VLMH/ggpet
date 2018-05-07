dc0beb46335a74cdd5830613f09fafd704e0bb4b
VLMH <victorboxster@gmail.com>
2018-05-08 07:14:18 +0800

    make breed and age required field

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,5 @@
 class Pet < ApplicationRecord
-  validates_presence_of :name, :species
+  validates_presence_of :name, :species, :breed, :age
   validates_inclusion_of :species, in: PET_SPECIES
-  validates_numericality_of :age, only_integer: true, allow_nil: true
+  validates_numericality_of :age, only_integer: true
 end
