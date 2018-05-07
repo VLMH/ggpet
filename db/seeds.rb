9d694d06bec345160b4965a7fbb0b60a387e8da3
VLMH <victorboxster@gmail.com>
2018-05-07 16:44:25 +0800

    preference should be nullable for no preference

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Pet.create([
+  { name: 'Dog_1', species: 'dog', breed: 'labrador', age: 2, available_at: '2018-05-07' },
+  { name: 'Dog_2', species: 'dog', breed: 'poodle', age: 3, available_at: '2018-05-08' },
+  { name: 'Dog_3', species: 'dog', breed: 'spaniel', age: 4, available_at: '2018-05-09' },
+  { name: 'Cat_1', species: 'cat', breed: 'abyssinians', age: 2, available_at: '2018-05-07' },
+  { name: 'Cat_2', species: 'cat', breed: 'bobtails', age: 3, available_at: '2018-05-08' },
+  { name: 'Cat_3', species: 'cat', breed: 'shorthair', age: 4, available_at: '2018-05-09' },
+])
+
+Customer.create([
+  { name: 'Pet Lover' },
+  { name: 'Dog Lover', preferences: { dog: {} } },
+  { name: 'Cat Lover', preferences: { cat: {} } },
+  { name: 'Dog n Cat Lover', preferences: { dog: {}, cat: {} } },
+  { name: 'Labrador Lover', preferences: { dog: { breed: ['labrador'] } } },
+  { name: 'Puppy Lover', preferences: { dog: { age_max: 1 } } },
+])
