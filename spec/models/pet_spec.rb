6f9090a63efbddb94e094d0946ad501e268b2f67
VLMH <victorboxster@gmail.com>
2018-05-09 16:36:02 +0800

    scope for available pets

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Pet, type: :model do
-  # validations
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:species) }
-  it { should validate_presence_of(:breed) }
-  it { should validate_presence_of(:age) }
-  it { should validate_inclusion_of(:species).in_array(PET_SPECIES) }
-  it { should validate_numericality_of(:age) }
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:species) }
+    it { should validate_presence_of(:breed) }
+    it { should validate_presence_of(:age) }
+    it { should validate_inclusion_of(:species).in_array(PET_SPECIES) }
+    it { should validate_numericality_of(:age) }
+  end
+
+  describe "scopes" do
+    describe "#available" do
+      before(:each) do
+        create(:pet)
+        create(:pet, available_at: (Time.now + 1.day).strftime('%F'))
+      end
+
+      it { expect(Pet.available.count).to eq(1) }
+    end
+  end
 end
