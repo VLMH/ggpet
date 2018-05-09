03c5f48fcb8bdd5be20d54fcc491a2026fd22d12
VLMH <victorboxster@gmail.com>
2018-05-09 19:01:47 +0800

    pet may have an adoptor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_09_100825) do
+ActiveRecord::Schema.define(version: 2018_05_09_105326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2018_05_09_100825) do
     t.datetime "available_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "adopted_by"
+    t.index ["adopted_by"], name: "index_pets_on_adopted_by"
     t.index ["species"], name: "index_pets_on_species"
   end
 
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Pet, type: :model do
   describe "associations" do
     it { should have_many(:adoptions) }
+    it { should belong_to(:adoptor) }
   end
 
   describe "validations" do
