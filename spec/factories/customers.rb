26c73b7c45a30c096bdae25c6c01f82f974ae046
VLMH <victorboxster@gmail.com>
2018-05-09 12:02:01 +0800

    remove no use factory

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -2,17 +2,4 @@ FactoryBot.define do
   factory :customer do
     name { Faker::Name.name }
   end
-
-  factory :customer_with_pref do
-    name { Faker::Name.name }
-    preferences do
-      {
-        dog: {
-          breed: [Faker::Dog.breed],
-          age_min: rand(10),
-          age_max: rand(10) + 10,
-        }
-      }
-    end
-  end
 end
