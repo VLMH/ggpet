e81a0f456e285446ef1cf39be637a733844f3de9
VLMH <victorboxster@gmail.com>
2018-05-09 16:16:22 +0800

    matching service to match pets for customer

--- app/services/matching_service.rb
+++ app/services/matching_service.rb
@@ -5,4 +5,37 @@ class MatchingService
       .or(Customer.like_age(pet.age).like_species(pet.species))
       .or(Customer.like_age(pet.age).like_breed(pet.species, pet.breed))
   end
+
+  def match_all_pet_by_customer(customer)
+    if (customer.preferences.nil?)
+      return Pet.where(age: customer.preference_age_min..customer.preference_age_max)
+    end
+
+    preferences = customer.preferences.clone
+
+    # first preference
+    first_species, first_species_pref = preferences.shift
+    query = Pet
+      .where(age: customer.preference_age_min..customer.preference_age_max)
+      .where(species: first_species)
+
+    if first_species_pref['breed']
+      query = query.where(breed: first_species_pref['breed'])
+    end
+
+    # remaining preferences
+    final_query = preferences.reduce(query) do |carry_query, (species, species_pref)|
+      q = Pet
+        .where(age: customer.preference_age_min..customer.preference_age_max)
+        .where(species: species)
+
+      if species_pref['breed']
+        q = q.where(breed: species_pref['breed'])
+      end
+    
+      carry_query.or(q)
+    end
+
+    final_query
+  end
 end
--- spec/services/matching_service_spec.rb
+++ spec/services/matching_service_spec.rb
@@ -64,4 +64,45 @@ RSpec.describe MatchingService, type: :service do
       expect(matching.size).to eq(3)
     end
   end
+
+  describe "#match_all_pet_by_customer" do
+    def matching(customer)
+      MatchingService.new.match_all_pet_by_customer(customer)
+    end
+
+    before(:each) do
+      create(:pet, species: "dog", breed: "poodle", age: 3)
+      create(:pet, species: "dog", breed: "labrador", age: 5)
+      create(:pet, species: "cat", breed: "balinese", age: 7)
+    end
+
+    it "returns pets for no preferences" do
+      customer = build(:customer)
+      expect(matching(customer).size).to eq(3)
+    end
+
+    it "returns pets for age max 4" do
+      customer = build(:customer, preference_age_max: 4)
+      expect(matching(customer).size).to eq(1)
+    end
+
+    it "returns all dogs" do
+      customer = build(:customer, preferences: {dog: {}})
+      result = matching(customer)
+      expect(result.size).to eq(2)
+      result.each { |pet| expect(pet.species).to eq("dog")}
+    end
+
+    it "returns dogs and cats that age min 4" do
+      customer = build(:customer, preferences: {dog: {}, cat: {}}, preference_age_min: 4)
+      result = matching(customer)
+      expect(result.size).to eq(2)
+      result.each { |pet| expect(pet.age).to be >= 4}
+    end
+
+    it "returns no pets matched" do
+      customer = build(:customer, preferences: {dog: {breed: ['labrador']}, cat: {}}, preference_age_max: 4)
+      expect(matching(customer).size).to eq(0)
+    end
+  end
 end
