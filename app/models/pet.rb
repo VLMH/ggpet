a7d30d1f92e190ca892a304ad1a4e6cd9a420179
VLMH <victorboxster@gmail.com>
2018-05-09 16:36:02 +0800

    scope for available pets

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,9 @@
 class Pet < ApplicationRecord
+  # validations
   validates_presence_of :name, :species, :breed, :age
   validates_inclusion_of :species, in: PET_SPECIES
   validates_numericality_of :age, only_integer: true
+
+  # scopes
+  scope :available, -> { where("available_at <= ?", Time.now.strftime('%F'))}
 end
