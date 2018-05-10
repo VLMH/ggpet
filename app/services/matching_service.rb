ec5605477ddb1271df484b417b9deff6bbbc907b
VLMH <victorboxster@gmail.com>
2018-05-10 23:15:57 +0800

    match customers that have no adoptions

--- app/services/matching_service.rb
+++ app/services/matching_service.rb
@@ -1,9 +1,9 @@
 class MatchingService
   def match_all_customer_by_pet(pet)
     Customer
-      .like_age(pet.age).without_preferences
-      .or(Customer.like_age(pet.age).like_species(pet.species))
-      .or(Customer.like_age(pet.age).like_breed(pet.species, pet.breed))
+      .no_adoptions.like_age(pet.age).without_preferences
+      .or(Customer.no_adoptions.like_age(pet.age).like_species(pet.species))
+      .or(Customer.no_adoptions.like_age(pet.age).like_breed(pet.species, pet.breed))
   end
 
   def match_all_pet_by_customer(customer)
