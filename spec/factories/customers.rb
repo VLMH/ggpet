c1e7d270f87f005958831adc0ae4d9181e7d3816
VLMH <victorboxster@gmail.com>
2018-05-10 23:08:25 +0800

    query customer with no adoptions

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :customer do
     name { Faker::Name.name }
+
+    factory :customer_with_adoption do
+      after(:create) do |customer, evaluator|
+        pet = create(:pet, adopted_by: customer.id)
+        customer.pets << pet
+      end
+    end
   end
 end
