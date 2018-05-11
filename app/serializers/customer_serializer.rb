e7bd928e3c1f8757755a45cb7b72fe36189df841
VLMH <victorboxster@gmail.com>
2018-05-11 09:01:46 +0800

    include adoptions info in response

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,3 +1,5 @@
 class CustomerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :preferences, :preference_age_min, :preference_age_max
+  attributes :id, :name, :preferences, :preference_age_min, :preference_age_max, :pets
+
+  has_many :pets
 end
