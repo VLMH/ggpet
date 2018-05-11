c4e3cd72bc89ea4209eaebed89f67e0ba76deff4
VLMH <victorboxster@gmail.com>
2018-05-11 09:01:46 +0800

    include adoptions info in response

--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -1,6 +1,8 @@
 class PetSerializer < ActiveModel::Serializer
   attributes :id, :name, :species, :breed, :age, :available_at
 
+  belongs_to :adoptor
+
   def available_at
     object.available_at ? object.available_at.strftime('%F') : nil
   end
