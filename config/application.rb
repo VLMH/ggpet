49a3f3c3ad816de087c4c554bc714afb862d4c5b
VLMH <victorboxster@gmail.com>
2018-05-13 15:26:12 +0800

    set sidekiq as active_job adapter

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module Ggpet
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Active Job
+    config.active_job.queue_adapter = :sidekiq
   end
 end
